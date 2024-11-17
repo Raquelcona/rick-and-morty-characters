cb222c2982eccaa5e129590b247a19e73d71d211
Raquelcona <raquelcolladosnavarro@gmail.com>
2024-11-17 19:49:13 +0100

    add

--- js/script.js
+++ js/script.js
@@ -5,32 +5,33 @@ const nextButton = document.getElementById('next-page');
 let currentPage = 1;
 let totalPages = 1;
 
-async function fetchCharacters(page) {
-  try {
-    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`);
-    const data = await response.json();   
+function fetchCharacters(page) {
+  fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+    .then(response => response.json())
+    .then(data   
+ => {
+      totalPages = data.info.pages;
+      characterList.innerHTML = ''; // Clear existing characters
 
+      data.results.forEach(character => {
+        const characterItem = document.createElement('li');
+        characterItem.innerHTML = `
+          <img src="${character.image}" alt="${character.name}">
+          <h2>${character.name}</h2>
+          <p>Species: ${character.species}</p>
+        `;
+        characterList.appendChild(characterItem);
+      });
 
-    totalPages = data.info.pages;   
-
-    characterList.innerHTML = ''; // Clear existing characters
-
-    data.results.forEach(character => {
-      const characterItem = document.createElement('li');
-      characterItem.innerHTML = `
-        <img src="${character.image}" alt="${character.name}">
-        <h2>${character.name}</h2>
-        <p>Species: ${character.species}</p>
-      `;
-      characterList.appendChild(characterItem);
+      prevButton.disabled = currentPage === 1;
+      nextButton.disabled = currentPage === totalPages;
+    })
+    .catch(error => {
+      console.error('Error fetching characters:', error);
     });
-
-    prevButton.disabled = currentPage === 1;
-    nextButton.disabled = currentPage === totalPages;
-  } catch (error) {
-    console.error('Error fetching characters:', error);
-  }
 }
+
+fetchCharacters(currentPage);
 
 fetchCharacters(currentPage);
 
